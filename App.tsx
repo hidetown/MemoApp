db11251e68dbb05c352a568c094e6585309d246a
pidemaro <66338159+hidetown@users.noreply.github.com>
2025-02-15 21:19:05 +0900

    Install ESLint

--- App.tsx
+++ App.tsx
@@ -1,7 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
 
-export default function App() {
+// アローファンクションにして型を宣言する。
+const App = (): JSX.Element => {
+  // 型を宣言する。
+  // let test: number = 1
+  // test = 2
+
+  // 関数も型宣言する。
+  // testFuncという定数に戻り値を代入する。
+  // const testFunc = (): string => {
+  //   return "test"
+  // }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!!!!!</Text>
@@ -15,6 +26,8 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
+
+export default App
